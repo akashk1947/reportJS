ffbcbe14d5360885e0fa0ef2ac5cce82fd6c8887
akashk1947 <theakash1947@gmail.com>
2026-03-31 21:43:08 +0530

    updated

--- links.mjs
+++ links.mjs
@@ -1,13 +1,5 @@
 const links = 
 `
-https://t.me/proxyinterviewjobsupport/168471
-https://t.me/qatestingindia/13124
-https://t.me/web_dev_support/67529
-https://t.me/testing_support/4192
-https://t.me/testing_support/4190
-https://t.me/testing_support/4187
-https://t.me/testing_support/4186
-https://t.me/testing_support/4182
-https://t.me/testing_support/4173
+https://t.me/phpdevelopersindian/23579
 `;
 export default links;
--- report3 copy/api.js
+++
@@ -1,3 +0,0 @@
-const api_id = 35001004
-const api_hash = "65f4e46a5842cdb7892da507bce98426"
-const session_name = "auto_send_session"
